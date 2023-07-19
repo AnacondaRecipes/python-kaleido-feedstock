6547a677dcbe6bd2d5c71ce33453de0042a4e48b
Serhii Kupriienko <skupriienko@anaconda.com>
2023-07-19 12:15:03 +0300

    python-kaleido 0.2.1

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,3 +1,8 @@
 from kaleido.scopes.plotly import PlotlyScope
+import subprocess
+
+
+subprocess.run(["pip", "check"], check=True)
+
 scope = PlotlyScope()
-assert scope.transform({"data": []}).startswith(b'\x89PNG')
+assert scope.transform({"data": []}).startswith(b"\x89PNG")
